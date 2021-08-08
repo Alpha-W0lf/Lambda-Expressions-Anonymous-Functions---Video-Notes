60139c9ad6b6e405ad55b60dd06329a6632525a5
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-08 13:28:48 -0500

    working

--- main.py
+++ main.py
@@ -17,4 +17,12 @@
 
 # https://www.youtube.com/watch?v=25ovCm9jKfA
 
-# Write function 
+# Write function to compute 3x + 1
+
+def f(x):
+    return 3*x + 1
+
+print(f(2))
+
+# Anonymous Functions = Lambda Expressions (they mean the same thing)
+
