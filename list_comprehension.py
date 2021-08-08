5d4c44169ba2a41c3d51a1e7f7d0714cb11bd5cc
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-08 18:58:26 -0500

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,2 +1,26 @@
 # https://www.youtube.com/watch?v=AhSvKGTh28Q
 
+# list: [1, 2, "a", 3.14]
+
+# list comprehensions:
+#     [expr for val in collection]
+#     [expr for val in collection if <test>]
+#     [expr for val in collection if <test1> and <test2>]
+#     [expr for val1 in collection1 and val2 in collection2]
+
+# squares = []
+# for i in range(1, 101):
+#     squares.append(i**2)
+#
+# print(squares)
+#
+# squares2 = [i**2 for i in range (1, 101)]
+# print(squares2)
+
+# remainders5 = [x**2 % 5 for x in range (1, 101)]
+# print(remainders5)
+#
+# remainders11 = [x**2 % 11 for x in range (1, 101)]
+# print(remainders11)
+
+movies = 
