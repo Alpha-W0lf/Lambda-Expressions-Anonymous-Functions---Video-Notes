418896ecc588f6f828191ce735cad7d9128b5df8
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-08 14:00:46 -0500

    Update lambda_functions.py

--- lambda_functions.py
+++ lambda_functions.py
@@ -41,3 +41,14 @@ scifi_authors = ["Isaac Asimov", "Ray Bradbury", "Robert Heinlein", "Arthur C. C
 
 scifi_authors.sort(key=lambda name: name.split(" ")[-1].lower())
 print(scifi_authors)
+
+def build_quadratic_function(a, b, c):
+    """Returns the function f(x) = ax^2 + bx + c"""
+    return lambda x : a*x**2 + b*x + c
+
+f = build_quadratic_function(2, 3, -5)
+print(f(0))
+print(f(1))
+print(f(2))
+
+print(build_quadratic_function(3, 0, 1)(2)) # 3x^2 + 1 evaluated for x=2
