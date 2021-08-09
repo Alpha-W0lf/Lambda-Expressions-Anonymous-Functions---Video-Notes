20f4a5c412a681c47c7937f54adfcc38be61d331
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-08 19:02:31 -0500

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -23,4 +23,17 @@
 # remainders11 = [x**2 % 11 for x in range (1, 101)]
 # print(remainders11)
 
-movies = 
+movies = ["Star Wars", "Ghandi", "Casablanca", "Shawshank Redemption", "Toy Story", "Gone with the Wind",
+          "Citizen Kane", "It's a Wonderful Life", "The Wizard of Oz", "Gattaca", "Rear Window", "Ghostbusters",
+          "To Kill A Mockingbird", "Good Will Hunting", "2001: A Space Odyssey", "Raiders of the Lost Ark",
+          "Groundhog Day", "Close Encounters of the Third Kind"]
+
+# gmovies = []
+# for title in movies:
+#     if title.startswith("G"):
+#         gmovies.append(title)
+
+# print(gmovies)
+
+gmovies = [title for title in movies if title.startswith("G")]
+print(gmovies)
