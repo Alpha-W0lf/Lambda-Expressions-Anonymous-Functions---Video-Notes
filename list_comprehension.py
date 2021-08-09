7007f85dd98fdeac86ddf17b3f9503816ac23e08
Tom Chacko <74878700+Alpha-W0lf@users.noreply.github.com>
2021-08-08 20:58:09 -0500

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -23,10 +23,10 @@
 # remainders11 = [x**2 % 11 for x in range (1, 101)]
 # print(remainders11)
 
-movies = ["Star Wars", "Ghandi", "Casablanca", "Shawshank Redemption", "Toy Story", "Gone with the Wind",
-          "Citizen Kane", "It's a Wonderful Life", "The Wizard of Oz", "Gattaca", "Rear Window", "Ghostbusters",
-          "To Kill A Mockingbird", "Good Will Hunting", "2001: A Space Odyssey", "Raiders of the Lost Ark",
-          "Groundhog Day", "Close Encounters of the Third Kind"]
+# movies = ["Star Wars", "Ghandi", "Casablanca", "Shawshank Redemption", "Toy Story", "Gone with the Wind",
+#           "Citizen Kane", "It's a Wonderful Life", "The Wizard of Oz", "Gattaca", "Rear Window", "Ghostbusters",
+#           "To Kill A Mockingbird", "Good Will Hunting", "2001: A Space Odyssey", "Raiders of the Lost Ark",
+#           "Groundhog Day", "Close Encounters of the Third Kind"]
 
 # gmovies = []
 # for title in movies:
@@ -35,5 +35,9 @@ movies = ["Star Wars", "Ghandi", "Casablanca", "Shawshank Redemption", "Toy Stor
 
 # print(gmovies)
 
-gmovies = [title for title in movies if title.startswith("G")]
-print(gmovies)
+# gmovies = [title for title in movies if title.startswith("G")]
+# print(gmovies)
+
+movies = [("Citizen Kane", 1941), ("Spirited Away", 2001), ("It's a Wonderful Life", 1946), ("Gattaca", 1997),
+          ("No Country for Old Men", 2007), ("Rear Window", 1954), ("The Lord of the Rings: The Fellowship of the "
+                                                                    "Ring", 2001), ("Groundhog Day", 1993),]
